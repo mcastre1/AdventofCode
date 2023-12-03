58a5890dc5a521c9157cb1b25004ec08a9dcb5bc
mcastre1 <miguelcastrejongal@gmail.com>
2023-12-02 21:42:12 -0700

    Part two completed by finding minimum number of cubes needed to make a game valid.

--- Day_two/main.py
+++ Day_two/main.py
@@ -1,4 +1,4 @@
-if __name__ == "__main__":
+def part_one():
     sum = 0
 
     with open("./Day_two/games.txt") as file:
@@ -25,28 +25,56 @@ if __name__ == "__main__":
                 if red > 12 or green > 13 or blue > 14:
                     valid = False
                     break
-
-                
-
                     
             if valid:
                 sum += int(game_id)
-
-            #pulls = [pull.strip() for pull in pulls]
-            #print(pulls)
-            # for pull in pulls:
-            #     num = int(pull.split(' ')[0].strip())
-            #     color = pull.split(' ')[1].strip()
-
-            
-
-            # print(game_id)
-            # print(f"red: {red}, green: {green}, blue: {blue}")
-            # if red <= 12 and green <= 13 and blue <= 14:
-            #     print(".")
-            #     sum += game_id
-
     
     print(sum)
+
+
+def part_two():
+    sum = 0
+
+    with open("./Day_two/games.txt") as file:
+        for line in file:
+            game_id = int(line.split(':')[0].split(' ')[1].strip())
+            sub_games = line.split(':')[1].strip().split(";")
+            valid = True
+            min_red = 0
+            min_green = 0
+            min_blue = 0
+
+            for sub_game in sub_games:
+                red = 0
+                green = 0
+                blue = 0
+                for pull in sub_game.split(','):
+                    pull = pull.strip()
+                    num = int(pull.split(' ')[0].strip())
+                    color = pull.split(' ')[1].strip()
+                    
+                    if color == 'red':
+                        red += num
+                    elif color == 'green':
+                        green += num
+                    elif color == 'blue':
+                        blue += num
+
+                if red >= min_red:
+                    min_red = red
+                
+                if green >= min_green:
+                    min_green = green
+                
+                if blue >= min_blue:
+                    min_blue = blue
+            
+            print(f"red: {min_red}, green: {min_green}, blue: {min_blue}")
+            sum += min_red * min_green * min_blue
+
+    print(sum)
+
+if __name__ == "__main__":
+    part_two()
 
 
